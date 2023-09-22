539ee3eed691c3e5d7901953b1fa6eccea7754b0
MorenoBello1 <arielmorenobellojeampierre@gmail.com>
2023-09-22 10:56:44 -0500

    finalizado

--- index.ts
+++ index.ts
@@ -7,5 +7,8 @@ const aroYamaha = new Aro("Bridgstone", "185/70R14 88T");
 const llantasYamaha = new Llantas("Bridgestone", "265 65r17", aroYamaha);
 const motorYamaha = new Motor("Yamaha", "WR450F WR 450 05");
 const yamaha=new Moto("Yamaha","Deportivas", motorYamaha, llantasYamaha, 2);
+console.log(yamaha.imprimir());
+
 
 const Honda=new Moto("Honda","Deportiva",motorYamaha,llantasYamaha,2);
+console.log(Honda.imprimir());
